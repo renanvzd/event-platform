edc5230839a92333ebac4b57024d6746f2fbd1b4
Renan Veronez Drechsler <renanvz@outlook.com>
2022-07-02 11:37:52 +0100

    Page Subscription - Create User on GraphQL

--- src/lib/apolo.ts
+++ src/lib/apolo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-eu-west-2.graphcms.com/v2/cl4zsb1rv0hlf01uec0mkftby/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
